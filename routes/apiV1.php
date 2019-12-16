6df2a4f86064de1f5f268859fc53e9eb6b6d7234
Prayugo Dwi <pray.arsian24@gmail.com>
2019-12-17 01:02:36 +0700

    update rooting
    update rooting

--- routes/apiV1.php
+++ routes/apiV1.php
@@ -8,9 +8,9 @@ $router->group(['prefix' => 'N/1', 'namespace' => 'Nanime'], function () use ($r
     $router->post('LastUpdateAnime', 'LastUpdateEpsAnimController@LastUpdateAnime');
     $router->post('ListAnime', 'ListAnimeController@ListAnime');
     $router->post('GenreListAnime', 'GenreListAnimeController@GenreListAnime');
-    $router->post('SearchAnime', 'SearchAnimeControoler@SearchAnime');
+    // $router->post('SearchAnime', 'SearchAnimeControoler@SearchAnime');
     $router->post('SingleListAnime', 'SingleListAnimeController@SingleListAnim');
-    $router->post('SearchGenreAnime', 'SearchGenreAnimeController@SearchGenreAnime');
+    // $router->post('SearchGenreAnime', 'SearchGenreAnimeController@SearchGenreAnime');
     $router->post('TrandingWeekAnime', 'TrandingWeekAnimeController@TrandingWeekAnime');
     $router->post('StreamAnime', 'StreamAnimeController@StreamAnime');
     $router->post('ScheduleAnime', 'ScheduleAnimeController@ScheduleAnime');
